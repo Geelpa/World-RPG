fd0de591b071d8210120249dc238be921165df1f
Cristian Curtinaz <curtinazif@gmail.com>
2022-09-01 13:50:19 -0300

    Registering function added

--- ajax/users.js
+++ ajax/users.js
@@ -1,90 +1,22 @@
 import { get, post, _delete, _get } from './ajax_base.js';
 
-function login(username, password, successFunction, errorFunction, ip = '') {
+function login(username, password, successFunction, errorFunction) {
   post('/users/login',
     {
       username: username,
-      password: password,
-      ip: ip
+      password: password
     },
     successFunction,
     errorFunction
   );
 }
 
-function deleteUser(successFunction, errorFunction) {
-  _delete('/api/Users', successFunction, errorFunction);
-}
-
-function changePassword(oldPassword, newPassword, successFunction, errorFunction) {
-  post('/api/Users/change-password',
+function register(username, password, email, successFunction, errorFunction) {
+  post('/users/register',
     {
-      oldPassword: oldPassword,
-      newPassword: newPassword
-    },
-    successFunction,
-    errorFunction,
-    true);
-}
-
-function getMyIp(successFunction, errorFunction) {
-  _get('https://api64.ipify.org/?format=json', {}, successFunction, errorFunction, true);
-}
-
-function getUser(successFunction, errorFunction) {
-  get('/api/Users', {}, successFunction, errorFunction, true);
-}
-
-function addAdditionalInfo(form, successFunction, errorFunction) {
-  post('/api/Users/additional-info', form, successFunction, errorFunction, true);
-}
-
-function addUser(
-  name,
-  email,
-  password,
-  role,
-  ref,
-  successFunction,
-  errorFunction) {
-  post('/api/Users',
-    {
-      name: name,
-      email: email,
+      username: username,
       password: password,
-      role: role,
-      refUserId: ref
-    },
-    successFunction,
-    errorFunction
-  );
-}
-
-function passwordRecovery(email, successFunction, errorFunction) {
-  post('/api/Users/forgot-password',
-    {
       email: email
-    },
-    successFunction,
-    errorFunction
-  );
-}
-
-function checkCodeIsValid(code, successFunction, errorFunction) {
-  post('/api/Users/is-code-valid',
-    {
-      code: code
-    },
-    successFunction,
-    errorFunction
-  );
-}
-
-function changePasswordWithCode(code, newPassword, successFunction, errorFunction) {
-  post('/api/Users/change-password-with-code',
-    {
-      code: code,
-      newPassword: newPassword
     },
     successFunction,
     errorFunction
@@ -93,13 +25,5 @@ function changePasswordWithCode(code, newPassword, successFunction, errorFunctio
 
 export {
   login,
-  addUser,
-  getUser,
-  addAdditionalInfo,
-  deleteUser,
-  changePassword,
-  passwordRecovery,
-  checkCodeIsValid,
-  changePasswordWithCode,
-  getMyIp
+  register
 }
